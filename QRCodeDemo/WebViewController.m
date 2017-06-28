72e61cb06f2d0fb6ce9cacb72c4cc94f9957ece6
纪宇伟 <jiyuwei@jiyuweideMacBook-Pro.local>
2017-06-28 23:39:20 +0800

    add progressLine

--- QRCodeDemo/WebViewController.m
+++ QRCodeDemo/WebViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "WebViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface WebViewController () <UIWebViewDelegate>
+@interface WebViewController () <WKNavigationDelegate,WKUIDelegate>
 
-@property(nonatomic,strong)UIWebView *webView;
+@property(nonatomic,strong)WKWebView *webView;
+@property(nonatomic,strong)CALayer *progresslayer;
 
 @end
 
@@ -19,17 +21,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.title = _urlStr;
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose target:self action:@selector(showTheURL)];
     
-    _webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    _webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
     _webView.backgroundColor = [UIColor whiteColor];
-    _webView.delegate = self;
-    _webView.scalesPageToFit = YES;
+    _webView.navigationDelegate = self;
+    [_webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
+    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_urlStr]]];
+    
     [self.view addSubview:_webView];
     
-     [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_urlStr]]];
+    UIView *progress = [[UIView alloc]initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 3)];
+    progress.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:progress];
+    
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(0, 0, 0, 3);
+    layer.backgroundColor = [UIColor colorWithRed:0.29 green:0.55 blue:0.99 alpha:1].CGColor;
+    [progress.layer addSublayer:layer];
+    self.progresslayer = layer;
 }
 
 -(void)showTheURL
@@ -38,11 +52,40 @@
     [alert show];
 }
 
-#pragma mark - UIWebViewDelegate
+#pragma mark - KVO
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
+    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        self.progresslayer.opacity = 1;
+        //不要让进度条倒着走...有时候goback会出现这种情况
+        if ([change[@"new"] floatValue] < [change[@"old"] floatValue]) {
+            return;
+        }
+        self.progresslayer.frame = CGRectMake(0, 0, self.view.bounds.size.width * [change[@"new"] floatValue], 3);
+        if ([change[@"new"] floatValue] == 1) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.progresslayer.opacity = 0;
+            });
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.progresslayer.frame = CGRectMake(0, 0, 0, 3);
+            });
+        }
+    }else{
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
 
--(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+#pragma mark - WKWebViewDelegate
+
+-(void)webView:(UIWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(nonnull NSError *)error
 {
     NSLog(@"error: %@",error);
+}
+
+#pragma mark - Dealloc
+
+-(void)dealloc
+{
+    [_webView removeObserver:self forKeyPath:@"estimatedProgress"];
 }
 
 #pragma mark - MemoryWarning
